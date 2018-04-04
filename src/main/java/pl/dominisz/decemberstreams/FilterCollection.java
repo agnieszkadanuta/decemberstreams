440247dba6c1a33d7e90b6914631a3ac42f9fe4f
Agnieszka Chudzicka <agnieszkadanuta.chudzicka@gmail.com>
2018-04-04 18:54:49 +0200

    add implementation to FilterCollection

--- src/main/java/pl/dominisz/decemberstreams/FilterCollection.java
+++ src/main/java/pl/dominisz/decemberstreams/FilterCollection.java
@@ -1,15 +1,23 @@
 package pl.dominisz.decemberstreams;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilterCollection {
 
     public static List<String> transform7(List<String> collection) {
-        return collection;
+        List<String> newCollection = new ArrayList<>();
+        for (String elements : collection) {
+            if(elements.length() < 4){
+                newCollection.add(elements);
+            }
+        }
+        return newCollection;
     }
 
     public static List<String> transform(List<String> collection) {
-        return collection;
+        return collection.stream().filter(s -> s.length() < 4).collect(Collectors.toList());
     }
 
 }
--- src/test/java/pl/dominisz/decemberstreams/FilterCollectionTest.java
+++ src/test/java/pl/dominisz/decemberstreams/FilterCollectionTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static pl.dominisz.decemberstreams.FilterCollection.transform;
+import static pl.dominisz.decemberstreams.FilterCollection.transform7;
 
 /*
 Filter collection so that only elements with less then 4 characters are returned.
@@ -17,7 +18,7 @@ public class FilterCollectionTest {
     public void transformShouldFilterCollection() {
         List<String> collection = asList("My", "name", "is", "John", "Doe");
         List<String> expected = asList("My", "is", "Doe");
-        assertEquals(transform(collection), expected);
+        assertEquals(expected, transform(collection));
     }
 
 }
