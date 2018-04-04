a1894299a3a0a4aa0445a926850ec7f74b1f1a93
Agnieszka Chudzicka <agnieszkadanuta.chudzicka@gmail.com>
2018-04-04 20:38:16 +0200

    add implementation to Kids

--- src/main/java/pl/dominisz/decemberstreams/Kids.java
+++ src/main/java/pl/dominisz/decemberstreams/Kids.java
@@ -3,15 +3,26 @@ package pl.dominisz.decemberstreams;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Kids {
 
     public static Set<String> getKidNames7(List<Person> people) {
-        return new HashSet<>();
+
+        Set<String> names = new HashSet<>();
+        for (Person person : people) {
+            int age = person.getAge();
+            if(age < 18) {
+                String name = person.getName();
+                names.add(name);
+            }
+        }
+        return names;
     }
 
     public static Set<String> getKidNames(List<Person> people) {
-        return new HashSet<>();
-    }
 
+        return people.stream().filter(person -> person.getAge() < 18)
+                .map(person -> person.getName()).collect(Collectors.toSet());
+    }
 }
--- src/test/java/pl/dominisz/decemberstreams/KidsTest.java
+++ src/test/java/pl/dominisz/decemberstreams/KidsTest.java
@@ -9,6 +9,7 @@ import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static pl.dominisz.decemberstreams.Kids.getKidNames;
+import static pl.dominisz.decemberstreams.Kids.getKidNames7;
 
 /*
 Get names of all kids (under age of 18)
@@ -16,17 +17,23 @@ Get names of all kids (under age of 18)
 public class KidsTest {
 
     @Test
-    public void getKidNameShouldReturnNamesOfAllKidsFromNorway() {
+    public void getKidNameShouldReturnNamesOfAllKids() {
         Person sara = new Person("Sara", 4);
         Person viktor = new Person("Viktor", 40);
         Person eva = new Person("Eva", 42);
         Person anna = new Person("Anna", 5);
         List<Person> collection = asList(sara, eva, viktor, anna);
-        Set<String> result = getKidNames(collection);
+        Set<String> result = getKidNames7(collection);
         assertTrue(result.contains("Sara"));
         assertTrue(result.contains("Anna"));
         assertFalse(result.contains("Viktor"));
         assertFalse(result.contains("Eva"));
+
+        Set<String> result2 = getKidNames(collection);
+        assertTrue(result2.contains("Sara"));
+        assertTrue(result2.contains("Anna"));
+        assertFalse(result2.contains("Viktor"));
+        assertFalse(result2.contains("Eva"));
     }
 
 }
